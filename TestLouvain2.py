48f71f9a111f66282951411816410f693b13ed9d
Billy Ren <renchengbin.billy@gmail.com>
2013-11-05 17:34:54 +0800

    karate_club_graph实验

--- TestLouvain2.py
+++ TestLouvain2.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 #better with karate_graph() as defined in networkx example.
 #erdos renyi don't have true community structure
-G = nx.erdos_renyi_graph(50, 0.05)
-#G = nx.karate_graph()
+#G = nx.erdos_renyi_graph(50, 0.05)
+G = nx.karate_club_graph()
 
 #first compute the best partition
 partition = community.best_partition(G)
@@ -20,7 +20,5 @@ for com in set(partition.values()) :
                                 if partition[nodes] == com]
     nx.draw_networkx_nodes(G, pos, list_nodes, node_size = 20,
                                 node_color = str(count / size))
-
-
 nx.draw_networkx_edges(G,pos, alpha=0.5)
 plt.show()
